8c146a957cae69af7c040a6bb85776b7c58d0b54
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2016-04-01 15:55:42 -0300

    Install simplecov
    This allows test coverage tracking through development.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter "/spec/"
+  add_filter "/features/"
+
+  coverage_dir 'coverage/rspec'
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'kolekti_metricfu'
