07e48ddcdcfdaae5da480fbba7fddb23df390374
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2016-04-11 16:58:06 -0300

    Create Parsers parse_all method
    This holds the logic for applying the correct parser for each wanted
    metric. It's highly based on KalibroProcessor v1.2.1.
    
    Signed-off-by: Diego Araújo <diegoamc@protonmail.ch>

--- lib/kolekti_metricfu/parsers.rb
+++ lib/kolekti_metricfu/parsers.rb
@@ -5,5 +5,15 @@ require 'kolekti_metricfu/parsers/flay'
 
 module KolektiMetricfu
   module Parsers
+    @parsers = { 'flog' => Flog, 'saikuro' => Saikuro, 'flay' => Flay }
+
+    def self.parse_all(results_yaml_path, wanted_metric_configurations, persistence_strategy)
+      parsed_result = YAML.load_file(results_yaml_path)
+
+      wanted_metric_configurations.each do |metric_configuration|
+        code = metric_configuration.metric.code.to_s
+        @parsers[code].parse(parsed_result[code.to_sym], metric_configuration, persistence_strategy)
+      end
+    end
   end
 end
