3c69f213a516b1e7940a979db3e3042344e76f70
Daniel Miranda <danielkza2@gmail.com>
2016-04-13 15:33:02 -0300

    Implement MetricFu Collector
    Signed-off-by: Diego Araújo <diegoamc@protonmail.ch>

--- lib/kolekti_metricfu/collector.rb
+++ lib/kolekti_metricfu/collector.rb
@@ -1,3 +1,5 @@
+require 'tempfile'
+
 module KolektiMetricfu
   class Collector < Kolekti::Collector
     def self.available?
@@ -12,6 +14,20 @@ module KolektiMetricfu
       )
 
       super('MetricFu', 'Set of Ruby metric tools', supported_metrics)
+    end
+
+    def collect_metrics(code_directory, wanted_metric_configurations, persistence_strategy)
+      tmp_file = Tempfile.new(['metric_fu', '.yml'], code_directory)
+      begin
+        Dir.chdir(code_directory) do
+          unless system(['metric_fu', '--format', 'yaml', '--out', tmp_file.path], [:out, :err] => '/dev/null')
+            raise Kolekti::CollectorError.new('MetricFu failed')
+          end
+        end
+        KolektiMetricfu::Parsers.parse_all(tmp_file.path, wanted_metric_configurations, persistence_strategy)
+      ensure
+        tmp_file.close!
+      end
     end
   end
 end
