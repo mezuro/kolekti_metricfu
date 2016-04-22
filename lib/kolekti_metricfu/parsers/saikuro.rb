60d0ac6d17bd502b4c0c6162af6624ac11107bea
Daniel Miranda <danielkza2@gmail.com>
2016-04-21 21:45:47 -0300

    Fix duplicated results in Saikuro as found with Noosfero
    It is necessary to manually filter duplicate methods and pick the latest
    one from the Saikuro results, as they'll contain multiple copies if they
    exist.

--- lib/kolekti_metricfu/parsers/saikuro.rb
+++ lib/kolekti_metricfu/parsers/saikuro.rb
@@ -2,13 +2,19 @@ module KolektiMetricfu
   module Parsers
     class Saikuro < Base
       def self.parse(collected_metrics_hash, metric_configuration, persistence_strategy)
+
+
         collected_metrics_hash[:files].each do |file|
           name_prefix = parse_file_name(file[:filename])
-          file[:classes].each do |clazz|
-            clazz[:methods].each do |method|
+          file[:classes].each do |cls|
+            # Filter duplicate methods, picking only the latest. Fortunately Hash::[] will pick the latest element
+            # when receiveing an array.
+            methods = Hash[cls[:methods].map { |method| [method[:name], method] }]
+            methods.each_value do |method|
               value = method[:complexity]
               name_suffix = module_name_suffix(method[:name])
               module_name = name_prefix + name_suffix
+
               granularity = KalibroClient::Entities::Miscellaneous::Granularity::METHOD
               persistence_strategy.create_tree_metric_result(metric_configuration, module_name, value.to_f, granularity)
             end
