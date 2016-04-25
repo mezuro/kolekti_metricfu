625c308b5aa13d142cb6a131f72c04a093f14117
Daniel Miranda <danielkza2@gmail.com>
2016-04-25 13:45:20 -0300

    Improve repository cloning acceptance steps
    Allow cloning a particular revision, and use the --depth option to
    reduce downloading time.

--- features/step_definitions/repository_steps.rb
+++ features/step_definitions/repository_steps.rb
@@ -1,8 +1,17 @@
 require 'tmpdir'
 
-Given(/^the "([^"]*)" repository is cloned$/) do |repository_url|
+def clone_repo(repository_url, revision=nil)
   @repository_path = Dir.mktmpdir
 
-  expect(system("git", "clone", "-q", repository_url, @repository_path, [:out, :err] => '/dev/null')).to be_truthy
+  cmd = ["git", "clone", "-q", "--depth", "1", repository_url, @repository_path]
+  cmd += ["-b", revision] if revision
+  expect(system(*cmd, [:out, :err] => '/dev/null')).to be_truthy
 end
 
+Given(/^the "([^"]*)" repository is cloned$/) do |repository_url|
+  clone_repo(repository_url)
+end
+
+Given(/^the "([^"]*)" repository is cloned with revision "([^"]*)"$/) do |repository_url, revision|
+  clone_repo(repository_url, revision)
+end
